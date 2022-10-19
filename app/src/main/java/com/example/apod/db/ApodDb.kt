2c6a3ff6a77ce5a5f6f6eb52958def7cd6077fa4
figlo <miro@figlar.sk>
2022-10-19 16:15:55 +0200

    removed @TypeConverters

--- app/src/main/java/com/example/apod/db/ApodDb.kt
+++ app/src/main/java/com/example/apod/db/ApodDb.kt
@@ -2,7 +2,6 @@ package com.example.apod.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(
     entities = [ApodDbModel::class],
@@ -10,7 +9,6 @@ import androidx.room.TypeConverters
     exportSchema = true,
 )
 
-@TypeConverters(Converters::class)
 abstract class ApodDb : RoomDatabase() {
     abstract val apodDao: ApodDao
 }
