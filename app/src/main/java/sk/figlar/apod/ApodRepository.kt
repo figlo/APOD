6a8c1a515e88d515a5a9ffae39d31c661e0b5af6
figlo <miro@figlar.sk>
2022-11-30 11:23:41 +0100

    added ApiModule.kt - dependency injection

--- app/src/main/java/sk/figlar/apod/ApodRepository.kt
+++ app/src/main/java/sk/figlar/apod/ApodRepository.kt
@@ -1,29 +1,19 @@
 package sk.figlar.apod
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import sk.figlar.apod.api.ApodApi
 import sk.figlar.apod.api.toDbModel
 import sk.figlar.apod.db.ApodDao
 import sk.figlar.apod.db.ApodDbModel
 import sk.figlar.apod.db.toApiModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 import timber.log.Timber
 import javax.inject.Inject
 
-class ApodRepository @Inject constructor(private val dao: ApodDao) {
-    private val apodApi: ApodApi
-
-    init {
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://api.nasa.gov/")
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-        apodApi = retrofit.create()
-    }
-
+class ApodRepository @Inject constructor(
+    private val dao: ApodDao,
+    private val apodApi: ApodApi,
+) {
     fun getDbApodsFlow(): Flow<List<ApodDbModel>> = dao.getDbApodsFlow()
 
     suspend fun getApod(apodId: Long): ApodDbModel? = dao.getDbApod(apodId)
---
+++ app/src/main/java/sk/figlar/apod/di/ApiModule.kt
@@ -0,0 +1,28 @@
+package sk.figlar.apod.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import sk.figlar.apod.api.ApodApi
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.nasa/gov/")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideApodApi(retrofit: Retrofit): ApodApi =
+        retrofit.create()
+}
