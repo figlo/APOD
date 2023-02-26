8935e314376d92a2fd9ac5868b271c5560fc8295
figlo <miro@figlar.sk>
2023-02-26 23:52:05 +0100

    fetch only pictures from last 15 days

--- app/src/main/java/sk/figlar/apod/ApodRepository.kt
+++ app/src/main/java/sk/figlar/apod/ApodRepository.kt
@@ -9,6 +9,9 @@ import sk.figlar.apod.db.ApodDbModel
 import sk.figlar.apod.db.toApiModel
 import sk.figlar.apod.db.toDomainModel
 import timber.log.Timber
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -23,12 +26,20 @@ class ApodRepository @Inject constructor(
 
     suspend fun refreshApods() {
         try {
-            val apiApods = apodApi.getApodApiModels()
+            val apiApods = apodApi.getApodApiModels(getStartDate())
             val dbApods = dao.getApodDbModelsFlow().first()
             val newApiApods = apiApods - dbApods.toApiModel().toSet()
             dao.insertAll(newApiApods.toDbModel())
         } catch (ex: Exception) {
             Timber.e("Failed to fetch gallery items: $ex")
         }
+    }
+
+    private fun getStartDate(): String {
+        val currentDate = LocalDate.now(ZoneId.systemDefault())
+        val dateFrom = currentDate.minusDays(14)
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        Timber.d(dateFrom.format(dateTimeFormatter))
+        return dateFrom.format(dateTimeFormatter)
     }
 }
--- app/src/main/java/sk/figlar/apod/api/ApodApi.kt
+++ app/src/main/java/sk/figlar/apod/api/ApodApi.kt
@@ -1,13 +1,15 @@
 package sk.figlar.apod.api
 
 import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.*
 
 private const val API_KEY = "ejIN7M9Yj2pqdvwYLGQlEC2rYaCbhDltzkjQcVj1"
 
 interface ApodApi {
+
     @GET(
-        "planetary/apod?api_key=$API_KEY" +
-        "&start_date=2023-01-15"
+        "planetary/apod?api_key=$API_KEY"
     )
-    suspend fun getApodApiModels(): List<ApodApiModel>
+    suspend fun getApodApiModels(@Query("start_date") startDate: String): List<ApodApiModel>
 }
